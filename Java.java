a58da3e6b1fd252b6904fe6d188df1e04842ab77
Tharindu Yehan <85949077+TYehan@users.noreply.github.com>
2023-10-02 20:52:58 +0530

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,6 @@
+//Hello World
+class Hacktoberfest {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Python.py
@@ -0,0 +1,2 @@
+#Hello World in Python
+print(f"Hello, World!")
