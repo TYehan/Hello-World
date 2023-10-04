6a53ef17e3f3419f2b81051f2ff608e8b2ce3ab5
Abhishek <msabhithakur7777@gmail.com>
2023-10-04 15:53:29 +0530

    added in js & py

--- JavaScript.js
+++ JavaScript.js
@@ -1,2 +1,3 @@
 console.log("Hello World");
 console.log("Hello World! from a contributor");
+console.log("Hello world");
--- Python.py
+++ Python.py
@@ -1,3 +1,4 @@
 #Hello World in Python
 print(f"Hello, World!")
 print(f"Hello, World from this contributor!")
+print("Hello,world")
